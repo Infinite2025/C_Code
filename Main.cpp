db5ffe4abd4d0e94cdad6f873cf613f7cd28c15f
Nguyen10220 <136629257+Nguyen10220@users.noreply.github.com>
2025-08-06 15:50:00 +0700

    add L6

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World! I'm Vietnamese" << endl;
     return 0;
     for(int i = 0; i < 5; ++i) {
         cout << i << " " << endl;
